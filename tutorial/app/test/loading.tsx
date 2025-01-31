f96a10bcb4287c05f9b7cd45e3d36b3f012eaac2
MinayeAliyeva <7s346pu@code.edu.az>
2025-01-31 16:09:09 +0400

    feat(loading):added loading component

---
+++ tutorial/app/test/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loading = () => {
+  return <div>Test Loading</div>;
+};
+
+export default Loading;
--- tutorial/app/test/page.tsx
+++ tutorial/app/test/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-
-const TestRoute = () => {
+async function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+const TestRoute = async() => {
+  await delay(3000);
   return <div>TestRoute</div>;
 };
 
